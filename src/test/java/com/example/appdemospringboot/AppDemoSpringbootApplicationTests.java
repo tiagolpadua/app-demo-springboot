1f5eae108ce0ebc64e2a28736c50d1a023187ecf
Tiago <tiagolpadua@bb.com.br>
2021-10-06 19:36:15 -0300

    commit inicial

---
+++ src/test/java/com/example/appdemospringboot/AppDemoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appdemospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDemoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
